73461053cd232193a0ab2ad13b4e9f38ba0e3ce1
Kosh <kosh20111@gmail.com>
2016-10-24 21:52:42 +0800

    rebasing app

--- app/src/main/assets/migrations/2.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE AppsModel ADD COLUMN countEntry INTEGER;
--- app/src/main/java/com/styleme/floating/toolbox/pro/widget/colorpicker/dashclockpicker/OnColorSelected.java
+++
@@ -1,9 +0,0 @@
-package com.styleme.floating.toolbox.pro.widget.colorpicker.dashclockpicker;
-
-/**
- * Created by kosh20111 on 5/27/2015. CopyRights @ Innov8tif
- */
-public interface OnColorSelected {
-
-    void onColorSelect(ColorSelector colorSelector);
-}
