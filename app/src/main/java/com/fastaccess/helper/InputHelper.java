a00e523f78c63aacea9ab5fec9be8d3043abecf7
Kosh <kosh20111@gmail.com>
2016-10-24 23:39:33 +0800

    removed leaking preference context and fixed restore crash where somehow int value become long.

--- app/src/main/java/com/fastaccess/helper/InputHelper.java
+++ app/src/main/java/com/fastaccess/helper/InputHelper.java
@@ -1,16 +1,10 @@
 package com.fastaccess.helper;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.text.TextUtils;
-import android.text.format.Formatter;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
 
 /**
  * Created by kosh20111 on 3/11/2015. CopyRights @ Innov8tif
@@ -44,10 +38,6 @@ public class InputHelper {
         return txt == null || isEmpty(txt.getEditText());
     }
 
-    public static String toNA(String value) {
-        return isEmpty(value) ? "N/A" : value;
-    }
-
     public static String toString(EditText editText) {
         return editText.getText().toString();
     }
@@ -62,23 +52,6 @@ public class InputHelper {
 
     @NonNull public static String toString(@NonNull Object object) {
         return !isEmpty(object) ? object.toString() : "";
-    }
-
-    public static String formatSize(Context context, long size) {
-        return Formatter.formatShortFileSize(context, size);
-    }
-
-    public static String formatPrice(double doubleValue) {
-        NumberFormat nf = NumberFormat.getCurrencyInstance();
-        DecimalFormatSymbols decimalFormatSymbols = ((DecimalFormat) nf).getDecimalFormatSymbols();
-        decimalFormatSymbols.setCurrencySymbol("");
-        ((DecimalFormat) nf).setDecimalFormatSymbols(decimalFormatSymbols);
-        return nf.format(doubleValue).trim();
-    }
-
-    public static String ordinal(int i) {
-        return i % 100 == 11 || i % 100 == 12 || i % 100 == 13 ? i + "th" : i +
-                new String[]{"th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"}[i % 10];
     }
 
     @NonNull public static String getTwoLetters(@NonNull String value) {
--- app/src/main/java/com/fastaccess/helper/PrefHelper.java
+++ app/src/main/java/com/fastaccess/helper/PrefHelper.java
@@ -1,10 +1,11 @@
 package com.fastaccess.helper;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import com.fastaccess.App;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,18 +14,7 @@ import java.util.Map;
  * Created by kosh20111 on 18 Oct 2016, 9:29 PM
  */
 public class PrefHelper {
-    private static PrefHelper prefHelper;
-    private Context context;
-
-    private PrefHelper(Context context) {
-        this.context = context;
-    }
-
-    public static void init(@NonNull Context context) {
-        if (prefHelper == null) {
-            prefHelper = new PrefHelper(context.getApplicationContext());
-        }
-    }
+    
 
     /**
      * @param key
@@ -39,17 +29,13 @@ public class PrefHelper {
             throw new NullPointerException("Key must not be null! (key = " + key + "), (value = " + value + ")");
         }
         Logger.e(key, value);
-        SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(prefHelper.context).edit();
+        SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(App.getInstance()).edit();
         if (value instanceof String) {
             edit.putString(key, (String) value);
-        } else if (value instanceof Integer) {
-            edit.putInt(key, (int) value);
-        } else if (value instanceof Long) {
-            edit.putLong(key, (long) value);
+        } else if (value instanceof Integer || value instanceof Long || value instanceof Float) {
+            edit.putInt(key, Integer.parseInt(value.toString()));
         } else if (value instanceof Boolean) {
             edit.putBoolean(key, (boolean) value);
-        } else if (value instanceof Float) {
-            edit.putFloat(key, (float) value);
         } else {
             edit.putString(key, value.toString());
         }
@@ -57,40 +43,32 @@ public class PrefHelper {
     }
 
     @Nullable public static String getString(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getString(key, null);
+        return PreferenceManager.getDefaultSharedPreferences(App.getInstance()).getString(key, null);
     }
 
     public static boolean getBoolean(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getBoolean(key, false);
+        return PreferenceManager.getDefaultSharedPreferences(App.getInstance()).getBoolean(key, false);
     }
 
     public static int getInt(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getInt(key, 0);
-    }
-
-    public static long getLong(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getLong(key, 0);
-    }
-
-    public static float getFloat(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getFloat(key, 0);
+        return PreferenceManager.getDefaultSharedPreferences(App.getInstance()).getInt(key, 0);
     }
 
     public static void clearKey(@NonNull String key) {
-        PreferenceManager.getDefaultSharedPreferences(prefHelper.context).edit().remove(key).apply();
+        PreferenceManager.getDefaultSharedPreferences(App.getInstance()).edit().remove(key).apply();
     }
 
     public static boolean isExist(@NonNull String key) {
-        return PreferenceManager.getDefaultSharedPreferences(prefHelper.context).contains(key);
+        return PreferenceManager.getDefaultSharedPreferences(App.getInstance()).contains(key);
     }
 
     public static void clearPrefs() {
-        PreferenceManager.getDefaultSharedPreferences(prefHelper.context).edit().clear().apply();
+        PreferenceManager.getDefaultSharedPreferences(App.getInstance()).edit().clear().apply();
     }
 
     public static Map<String, Object> getAll() {
         Map<String, Object> toBackupMap = new HashMap<>();
-        Map<String, ?> prefs = PreferenceManager.getDefaultSharedPreferences(prefHelper.context).getAll();
+        Map<String, ?> prefs = PreferenceManager.getDefaultSharedPreferences(App.getInstance()).getAll();
         for (String key : prefs.keySet()) {
             if (!InputHelper.isEmpty(key) && !key.equalsIgnoreCase("null")) {// sometimes key is null, for no fucking reason.
                 toBackupMap.put(key, prefs.get(key));
